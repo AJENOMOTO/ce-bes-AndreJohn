3620bf10a10544463c290be50afbd70df8ed23e6
AJENOMOTO <07308596@dwc-legazpi.edu>
2024-01-31 12:00:43 +0800

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+    /*
+     * Author:Andre John Miraran
+     * Date:January 25,2024
+     * Project:Activity 1 - Hello World
+     * 
+     * Note: Do not modify or delete this, thank you!
+     */
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+    }
+    
+}
